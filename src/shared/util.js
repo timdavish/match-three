7998d0f3ffe8db9f1b2e453a13f4e71304dc87d2
Tim Davis <timdavish@gmail.com>
2017-11-16 12:05:15 -0800

    chore(util): Add equalOnProps function
    - equalOnProps checks two objects for equality on supplied properties
    array
    - If no properties array is passed, checks for deep equality

--- src/shared/util.js
+++ src/shared/util.js
@@ -118,6 +118,25 @@ export function deepEqual(a, b) {
 }
 
 /**
+ * Checks two objects for equality on supplied properties
+ * @param {Object} a First value
+ * @param {Object} b First value
+ * @param {Array} props (Optional) Properties to check equality on
+ * @return {Boolean} Whether the two values are equal on the supplied properties or not
+ */
+export function equalOnProps(a, b, props = null) {
+  if (!isObject(a) || !isObject(b)) {
+    return false;
+  }
+
+  if (isBlank(props)) {
+    return deepEqual(a, b);
+  }
+
+  return props.every(p => deepEqual(a[p], b[p]));
+}
+
+/**
  * Restrict the given number between the given min and max
  * @param {Number} min Minimum value for n to be
  * @param {Number} n Value to be clamped
