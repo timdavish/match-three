c82ca38c315e6e45147f15ac68ff4fcdfc4e063c
Tim Davis <timdavish@gmail.com>
2017-11-16 10:23:41 -0800

    refactor(permutation-exception): Create throwable PermutationException
    - This is used when there are no possible permutations of the board that will
    result in a possible move

---
+++ src/exceptions/PermutationException.js
@@ -0,0 +1,6 @@
+function PermutationException() {
+  this.name = 'PermutationException';
+  this.message = 'No permutation of the board results in a possible move.';
+}
+
+export default PermutationException;
